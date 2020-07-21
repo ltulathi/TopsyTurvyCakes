2b55ea68aba1ee06ae81e34311eb3201d005475a
Linda T <ltulathi2@yahoo.com>
2020-07-20 20:02:10 -0700

    Process an uploaded image file

--- TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
+++ TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TopsyTurvyCakes.Models;
@@ -25,6 +26,9 @@ namespace TopsyTurvyCakes.Pages.Admin
         [BindProperty]
         public Recipe Recipe { get; set; }
        
+        [BindProperty]
+        public IFormFile Image { get; set; }
+
         public AddEditRecipeModel(IRecipesService recipesService)
         {
             this.recipesService = recipesService;
@@ -36,7 +40,23 @@ namespace TopsyTurvyCakes.Pages.Admin
 
         public async Task<IActionResult> OnPostAsync()
         {
-            Recipe.Id = Id.GetValueOrDefault();
+            var recipe = await recipesService.FindAsync(Id.GetValueOrDefault()) 
+                ?? new Recipe();
+
+            recipe.Name = Recipe.Name;
+            recipe.Description = Recipe.Description;
+            recipe.Ingredients = Recipe.Ingredients;
+            recipe.Directions = Recipe.Directions;
+
+            if (Image != null) {
+                using (var stream = new System.IO.MemoryStream())
+                {
+                    await Image.CopyToAsync(stream);
+                    recipe.Image = stream.ToArray();
+                    recipe.ImageContentType = Image.ContentType;
+                }
+            }
+
             await recipesService.SaveAsync(Recipe);
             return RedirectToPage("/Recipe", new { id = Id });
         }
